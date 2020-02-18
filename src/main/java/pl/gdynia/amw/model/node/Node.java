b51dd54da8d02685e5ffdf644d334c5ef3db4b7c
Jakub Bednarski <jakub.bednarski@gmail.com>
2020-02-19 00:11:59 +0100

    Adding functionality to mange multiple environments

--- src/main/java/pl/gdynia/amw/model/node/Node.java
+++ src/main/java/pl/gdynia/amw/model/node/Node.java
@@ -3,6 +3,7 @@ package pl.gdynia.amw.model.node;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import pl.gdynia.amw.model.CordaObject;
 
 import java.util.HashMap;
@@ -12,5 +13,6 @@ import java.util.Map;
 @EqualsAndHashCode
 public class Node implements CordaObject {
 
+    @Getter @Setter private String name;
     @Getter private Map<String, Object> properties = new HashMap<>();
 }
--- src/main/java/pl/gdynia/amw/service/NodeGenerator.java
+++ src/main/java/pl/gdynia/amw/service/NodeGenerator.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import pl.gdynia.amw.file.Storage;
 import pl.gdynia.amw.model.node.Node;
+import pl.gdynia.amw.model.node.NotaryNode;
 
 import java.util.Collection;
 import java.util.Map;
@@ -16,20 +17,35 @@ public class NodeGenerator {
     public void generateDeployNodesTask(String destination, Collection<Node> nodes) {
         String deployNodesTask = TemplateManager.getInstance().readTemplate("deployNodesTask.template");
         String singleNode = TemplateManager.getInstance().readTemplate("deployNodesTask_node.template");
+        String notarySection = TemplateManager.getInstance().readTemplate("deployNodesTask_nodeNotarySection.template");
 
         String nodesStr = "";
         for (Node node : nodes) {
-            String currentNode = new String(singleNode);
-            for (Map.Entry<String, Object> entry : node.getProperties().entrySet()) {
-                String key = entry.getKey();
-                Object value = entry.getValue();
-                String tag = getTag(key);
-                currentNode = currentNode.replaceAll(tag, getValueAsString(tag, value));
-            }
+            String currentNode = singleNode;
+            currentNode = currentNode.replaceAll("<!--notary-->", node instanceof NotaryNode ? notarySection : "");
+            currentNode = populateTemplateWithTags(currentNode, node.getProperties());
             nodesStr = nodesStr + "\n" + currentNode;
+            generateNodeConfigFiles(destination, node);
         }
 
-        Storage.getInstance().storeFile(destination, deployNodesTask.replaceAll("<!--nodes-->", nodesStr), "deployNodesTask.gradle");
+         Storage.getInstance().storeFile(destination, deployNodesTask.replaceAll("<!--nodes-->", nodesStr), "deployNodesTask.gradle");
+    }
+
+    public void generateNodeConfigFiles(String destination, Node node) {
+        String nodeConfig = TemplateManager.getInstance().readTemplate("nodeConfig.template");
+        nodeConfig = populateTemplateWithTags(nodeConfig, node.getProperties());
+        Storage.getInstance().storeFile(destination, nodeConfig, node.getName()+".config");
+    }
+
+    private String populateTemplateWithTags(String template, Map<String, Object> tagsAndValues) {
+        for (Map.Entry<String, Object> entry : tagsAndValues.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            String tag = getTag(key);
+            template = template.replaceAll(tag, getValueAsString(tag, value));
+        }
+
+        return template;
     }
 
     private String getValueAsString(String tag, Object object) {
